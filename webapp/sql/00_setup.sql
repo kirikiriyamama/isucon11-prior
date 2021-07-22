041bc1cb31876daf369d4d77ce1cfcf22a4e4256
kirikiriyamama <kirikiriyamama@gmail.com>
2021-07-22 18:15:35 +0900

    mysql につなげなくてかなしかった
    ちょっと雑

--- webapp/sql/00_setup.sql
+++ webapp/sql/00_setup.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS `isucon2021_prior`;
 CREATE DATABASE IF NOT EXISTS `isucon2021_prior` DEFAULT CHARACTER SET utf8mb4;
-CREATE USER IF NOT EXISTS `isucon`@`localhost` IDENTIFIED WITH mysql_native_password BY 'isucon';
-GRANT ALL ON `isucon2021_prior`.* TO `isucon`@`localhost`;
+CREATE USER IF NOT EXISTS `isucon`@`ip-172-31-%-%.ap-northeast-1.compute.internal` IDENTIFIED WITH mysql_native_password BY 'isucon';
+GRANT ALL ON `isucon2021_prior`.* TO `isucon`@`ip-172-31-%-%.ap-northeast-1.compute.internal`;
 
 set global slow_query_log = 1;
 set global slow_query_log_file = '/var/log/mysql/slow.log';
